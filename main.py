0edfae45d34859acc12c7fa593c4e2f747ad12a9
ng <nickgardner7777@gmail.com>
2022-10-24 19:23:03 -0400

    Change weekly plotting to 4 datapoints per day. Add file comments for clarity.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+File: main.py
+Author: Nick Gardner <nag6650@rit.edu>
+Script run by Google Cloud Run to scrape RIT's recreation website for gym data,
+store that data in a Google Firestore NoSQL database, then render the results with plotly.
+This produces a figure that is available online and is embedded in my website.
+"""
+
 import requests
 import datetime
 import pytz
@@ -17,11 +25,18 @@ import plotly.graph_objects as go
 
 # Constants
 RIT_GYM_URL = "https://recreation.rit.edu/facilityoccupancy"
+
+# These credentials files are created during the walkthrough setup for both Firebase and Plotly
 FIREBASE_CREDENTIALS_FILE = "firebase_credentials.json"
 PLOTLY_CREDENTIALS_FILE = "plotly_credentials.json"
 
 
 def get_counts(url):
+    """
+    Simple scraping function. Returns the current occupancy of the three areas of the gym.
+    :param url: Website to scrape (RIT gym website)
+    :return: list containing [ll_count, ul_count, aq_count]
+    """
     req = requests.get(url)
     page = str(req.content, "windows-1250")
     soup = BeautifulSoup(page, 'html.parser')
@@ -33,6 +48,12 @@ def get_counts(url):
 
 
 def store_counts(counts, firebase_cred_file):
+    """
+    Stores scraped occupancy counts in a firestore database
+    :param counts: Scraped counts
+    :param firebase_cred_file: Credentials file to log in to database
+    :return: None
+    """
     if not firebase_admin._apps:
         cred = credentials.Certificate(firebase_cred_file)
         default_app = firebase_admin.initialize_app(cred)
@@ -43,13 +64,19 @@ def store_counts(counts, firebase_cred_file):
     cur_time = datetime.datetime.now()
     format_time = str(pd.Timestamp(cur_time))
 
-    res = collection.document(format_time).set({
+    collection.document(format_time).set({
         'll_count': counts[0], 'ul_count': counts[1],
         'aq_count': counts[2],
     })
 
 
 def render_plot(plotly_cred_file, firebase_cred_file):
+    """
+    Renders figure with gym data.
+    :param plotly_cred_file: Credential file to send to chart studio to plot online
+    :param firebase_cred_file: Credential file to read data from Firebase
+    :return: None
+    """
     cred_file = open(plotly_cred_file)
     creds = json.load(cred_file)
     chart_studio.tools.set_credentials_file(username=creds["username"], api_key=creds["api_key"])
@@ -87,7 +114,7 @@ def render_plot(plotly_cred_file, firebase_cred_file):
     df_daily = df[(df['date'] >= today_begin) & (df['date'] <= tomorrow_begin)]
 
     now = datetime.datetime.now()
-    monday = now - datetime.timedelta(days=2)
+    monday = now - datetime.timedelta(days=now.weekday())
     week_begin = pd.Timestamp(tz=pytz.timezone('US/Eastern'), year=monday.year,
                               month=monday.month, day=monday.day)
     next_monday = monday + datetime.timedelta(days=7)
@@ -95,7 +122,7 @@ def render_plot(plotly_cred_file, firebase_cred_file):
                             month=next_monday.month, day=next_monday.day)
 
     df_weekly = df[(df['date'] >= week_begin) & (df['date'] <= week_end)]
-    df_weekly = df_weekly.groupby(pd.Grouper(key='date', freq='D')).mean().reset_index()
+    df_weekly = df_weekly.set_index('date').resample('6h').mean().reset_index()
 
     fig = go.Figure()
 
@@ -129,13 +156,20 @@ def render_plot(plotly_cred_file, firebase_cred_file):
     fig.update_layout(title="RIT Recreation Facility Occupancy", xaxis_title="Datetime",
                       yaxis_title="Number of Occupants", legend_title="Facility Name")
 
-    py.plot(fig, filename='RIT_gym_occupancy', auto_open=False)
+    py.plot(fig, filename='RIT_gym_occupancy_test', auto_open=False)
 
 
 def main(event_data, context):
+    """
+    Main function wrapper (required by Google Cloud Run)
+    :param event_data: Required parameter for Google Cloud Run
+    :param context: Required parameter for Google Cloud Run
+    :return: None
+    """
     counts = get_counts(RIT_GYM_URL)
     store_counts(counts, FIREBASE_CREDENTIALS_FILE)
     render_plot(PLOTLY_CREDENTIALS_FILE, FIREBASE_CREDENTIALS_FILE)
 
 
-main("blah", "bleh")
+# Uncomment this to run locally
+# main("blah", "bleh")
